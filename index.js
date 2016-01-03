28dc3df82e4a94a4bf284e68a01eeae07480a7ec
珈文 <jiawen.czq@alibaba-inc.com>
2016-01-03 23:33:32 +0800

    init

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
 var koa = require('koa');
 var app = koa();
 var path = require('path');
-// Must be used before any router is used
 var render = require('koa-ejs');
 
-var rt = require('koa-route');
+var routeList = ['/pets/:name', '/pets2/:name'];
+
+
+var router = require('koa-router')();
+
+
 
 render(app, {
-  root: path.join(__dirname, 'views'),
-  layout: 'template',
+  root: path.join(__dirname, 'app'),
+  layout: 'layout',
   viewExt: 'html',
   cache: false,
   debug: true
 });
 
-
+/*
 app.use(function *() {
   var users = [{name: 'Dead Horse'}, {name: 'Jack'}, {name: 'Tom'}];
   yield this.render('content', {
     users: users
   });
+});*/
+
+var pets = {
+  list: function*() {
+    var names = Object.keys(db);
+    this.body = 'pets: ' + names.join(', ');
+  },
+
+  show: function*(name) {
+    this.body = 'test';
+  },
+  test: function*() {
+    var users = [{
+      name: 'Dead Horse'
+    }, {
+      name: 'Jack'
+    }, {
+      name: 'Tom'
+    }];
+    yield this.render('content/content', {
+      users: users
+    });
+  }
+};
+
+//app.use(rt.get('/pets/:name', pets.show));
+
+
+routeList.forEach((_route, index) => {
+  router.get(_route, pets.test);
+
 });
+
+
+
+app
+  .use(router.routes())
+
 
 if (process.env.NODE_ENV === 'test') {
   module.exports = app.callback();
@@ -29,6 +70,6 @@ if (process.env.NODE_ENV === 'test') {
   console.log('open http://localhost:7001')
 }
 
-app.on('error', function (err) {
+app.on('error', function(err) {
   console.log(err.stack)
 });
